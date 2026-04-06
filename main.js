f4e30ef8306432915ce57f6ef07b63d990247e7c
ZenithAurora <2985395460@qq.com>
2026-04-06 17:24:20 +0800

    画了一个dio图进行dom增删改查操作的检验reconciler是否正确，并调整了样式

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 import MiniReact from "./MiniReact";
 
 const element = MiniReact.createElement(
-    'h1',
-    { id: 'title', style: "background:red" },
-    MiniReact.createTextElement('Hello World'),
-    MiniReact.createElement('div', { id: 'name', style: "background:blue;" }, '—MiniReact'),
-    MiniReact.createElement('button', { onClick: () => alert('hi') }, 'Click me!')
+    'div',
+    { id: 'title', style: "background: pink; font-size:20px; padding: 10px" },
+    MiniReact.createTextElement('react is powerful!'),
+    MiniReact.createElement('div', { style: 'text-align: right;' }, '—MiniReact'),
 )
 
 const container = document.querySelector('#root');
