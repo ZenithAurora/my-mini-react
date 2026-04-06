416a21999d57df583ad615c9271f65478ae5ba20
ZenithAurora <2985395460@qq.com>
2026-04-06 16:01:06 +0800

    Reconcilation的实现,只更新变动部分

--- MiniReact/render.js
+++ MiniReact/render.js
@@ -1,28 +1,194 @@
 import createDOM from './createDOM';
 
-
+// --------------------------（1）全局引用---------------------------------
 // 下一个要执行的任务（fiber）
 let nextUnitOfWork = null
 // 工作中的根节点
 let workInProgressRoot = null
+// 前一个完成的root
+let currentRoot = null
+// 记录要删除的fiber节点
+let deletion = []
+// ------------------------------------------------------------------
 
-// 发出第一个fiber
-// 调试用：控制台输入 printFiberTree() 即可查看完整 fiber 树（原生对象，可逐层展开）
+
+
+// --------------------------（2）render函数---------------------------------
 function render(element, container) {
     // 这个就是root的fiber
     workInProgressRoot = {
-        fiberName: 'root fiber', // 自己随便写的，方便在浏览器控制台调试的时候知道这是哪个fiber节点
         dom: container,
-        props: {
-            children: [element]
-        },
+        props: { children: [element] },
         sibling: null,
-        parent: null
+        parent: null,
+        child: null,
+        alternate: currentRoot
     }
 
     nextUnitOfWork = workInProgressRoot
 }
+//-----------------------------------------------------------------------
 
+
+
+// -----------------------（3）同步提交阶段-----------------------
+// 提交根Fiber
+function commitRoot() {
+    // 对于收集到的删除Fiber，提交的时候统一删除
+    deletion.forEach(commitWork)
+    deletion = [] // 清空
+    commitWork(workInProgressRoot.child)
+    currentRoot = workInProgressRoot
+    workInProgressRoot = null // 清空   
+}
+
+// 从 fiber 开始提交
+function commitWork(fiber) {
+    if (!fiber) return;
+
+    const parentDOM = fiber.parent.dom
+
+    // -------------------根据effectTag做不同的DOM操作-------------------
+    // （1）如果是替换，则替换老的DOM节点
+    if (fiber.effectTag === 'PLACEMENT' && fiber.dom) {
+        parentDOM.appendChild(fiber.dom)
+        // 对于新创建的节点，也需要注册事件监听器
+        updateDOM(fiber.dom, {}, fiber.props)
+    }
+    // （2）如果是删除，则从DOM中删除
+    else if (fiber.effectTag === 'DELETION' && fiber.dom) {
+        parentDOM.removeChild(fiber.dom)
+    }
+    // （3）如果是更新，则单独处理
+    else if (fiber.effectTag === 'UPDATE' && fiber.dom) {
+        updateDOM(fiber.dom, fiber.alternate.props, fiber.props)
+    }
+    // ----------------------------operation end--------------------------
+
+    // 递归 child 和 sibling
+    commitWork(fiber.child)
+    commitWork(fiber.sibling)
+}
+// --------------------------------------------------------------
+
+
+
+// ---------------- （4）updateDOM ----------------
+function updateDOM(dom, prevProps, nextProps) {
+    /**
+     * 举个例子
+     *  prevProps = {id: 'a', className: 'red', onClick, children: []}
+     *  nextProps = {className: 'blue', style: 'bold', onChange,children: []}
+     * 
+     * （1）删掉旧属性：id
+     * （2）更新props：className, style
+     */
+
+    //-------------------【1】事件监听器处理🆕-------------------
+    const isEvent = key => key.startsWith('on')               // 是否以on开头
+    const getEventName = key => key.slice(2).toLowerCase()   // onClick -> click
+
+    // [1] 移除旧的事件函数
+    Object.keys(prevProps)
+        .filter(isEvent)
+        .filter(key => !(key in nextProps) || prevProps[key] !== nextProps[key])
+        .forEach(key => {
+            const eventName = getEventName(key)
+            dom.removeEventListener(eventName, prevProps[key])
+        })
+
+    // [2] 添加新的事件函数
+    Object.keys(nextProps)
+        .filter(isEvent)
+        .filter(key => !(key in prevProps) || prevProps[key] !== nextProps[key])
+        .forEach(key => {
+            const eventName = getEventName(key)
+            dom.addEventListener(eventName, nextProps[key])
+        })
+    //-------------------🆕事件监听器处理end🆕-----------------
+
+
+
+    // -------------------【2】属性（非事件函数）-------------------
+
+    // （1）对老Props：删掉那些旧的属性（child除外）
+    Object.keys(prevProps)
+        .filter(key => key !== 'children')     // ① → {id, className}
+        .filter(key => !(key in nextProps))      // ② → {id}
+        .forEach(key => delete dom[key])       // ③ → 删除id属性
+
+    // （2）赋予dom新的Props或者改变的Props
+    Object.keys(nextProps)
+        .filter(key => key !== 'children')                                         // ① → {className, style}
+        .filter(key => !(key in prevProps) || prevProps[key] !== nextProps[key])     // ② → {className，style}
+        .forEach(key => dom[key] = nextProps[key])                                 // ③ → red更新为blue，新增style
+}
+// ---------------------------------------------------------------
+
+
+
+// ---------------------------（5）reconcile函数--------------------------------
+function reconcileChildren(wipFiber, elements) {
+    // wipFiber == workInProgressFiber
+    let index = 0
+    let prevSibling = null
+    // 这里需要判断一下alternate是否存在，因为初始阶段wipFiber.alternate(旧Fiber)为null
+    let oldFiber = wipFiber.alternate ? wipFiber.alternate.child : null
+
+    while (index < elements.length || oldFiber != null) {
+        const element = elements[index]
+        // 两颗DOM一样：新元素存在 并且 老元素也存在 并且 新元素的type跟老元素的type一样
+        const sameType = element && oldFiber && element.type === oldFiber.type
+        let newFiber = null
+
+        // ---------------针对 更新、新建、删除三种情况分别处理---------------
+        // （1）更新：类型一样
+        if (sameType) {
+            newFiber = {
+                type: oldFiber.type,
+                props: element.props,   //👈== 类型一样，直接用新元素的props就行
+                dom: oldFiber.dom,      //👈== 复用老 dom
+                parent: wipFiber,
+                alternate: oldFiber,
+                effectTag: 'UPDATE',    //👈== 标记为更新
+            }
+        }
+
+        // （2）新建：新元素存在 老元素不存在
+        if (!sameType && element) {
+            newFiber = {
+                type: element.type,
+                props: element.props,
+                dom: null,              //👈== 新建的，所以还没有DOM
+                parent: wipFiber,
+                alternate: null,        //👈== 没有老Fiber，所以为null
+                effectTag: 'PLACEMENT', //👈== 标记为新建
+            }
+        }
+
+        // （3）删除：老元素存在 新元素不存在
+        if (!sameType && oldFiber) {
+            oldFiber.effectTag = 'DELETION' //👈 标记为删除
+            deletion.push(oldFiber)         //👈 收集待删除的Fiber（后续Commit阶段处理）
+        }
+        // ------------------------------end----------------------------
+
+        // 将performUnitOfWork中建立fiber之间链条的逻辑写道这里来
+        if (index === 0) wipFiber.child = newFiber // 第一个为children
+        else prevSibling.sibling = newFiber        // 后续为sibling
+
+        // 更新状态
+        if (oldFiber) oldFiber = oldFiber.sibling
+        prevSibling = newFiber
+        index++
+    }
+}
+// ----------------------------------------------------------------------------
+
+
+
+
+//----------------（6）performUnitOfWork执行工作单元----------------
 /**
  * 执行任务: TODO 3 things
  * 1. add the element to the DOM
@@ -30,63 +196,16 @@ function render(element, container) {
  * 3. select the next unit of work
  */
 function performUnitOfWork(fiber) {
-    /**
-     * 第一次的时候，进来的fiber是 root 的fiber
-     * fiber = {
-     *  dom: container,
-     *  props: { children: [h1] },
-     *  sibling: null,
-     *  parent: null
-     * }
-     * 
-     * <div class="root">
-     *   <h1>
-     *     hello world
-     *     <div>—MiniReact</div>
-     *   </h1>
-     * </div>
-     *
-     */
-
-
     // ==================== 1.创建DOM并记录到当前fiber节点中 ====================
     if (!fiber.dom) {
         fiber.dom = createDOM(fiber)
     }
     // 追加到父节点
-    if (fiber.parent) {
-        fiber.parent.dom.appendChild(fiber.dom)
-    }
+    // if (fiber.parent) fiber.parent.dom.appendChild(fiber.dom)
 
     // ==================== 2.创建子级Fiber以及建立关系 ====================
     const elements = fiber.props.children
-    let prevSibling = null
-
-    // 把每一个孩子元素，弄成一个fiber
-    // 构建fiber之间的关系
-    for (let childrenElement of elements) {
-        const newFiber = {
-            fiberName: `${childrenElement.type} fiber`, // 自己随便写的，方便在浏览器控制台调试的时候知道这是哪个fiber节点
-            type: childrenElement.type,
-            props: childrenElement.props,
-            parent: fiber, // 新造出来的这个fiber直接连到父亲身上
-            child: null,
-            dom: null,
-            sibling: null
-        }
-
-        // 最开始没有孩子，所以这个新的fiber就是太子
-        // fiber只认太子
-        // 至于fiber的二儿子，那么直接认太子当兄弟（等级森严）
-        // 同样，对于三儿子，只认二儿子当兄弟
-        if (!fiber.child) {
-            fiber.child = newFiber
-        } else {
-            prevSibling.sibling = newFiber
-        }
-        prevSibling = newFiber // 记录上一个哥哥是谁
-    }
-
+    reconcileChildren(fiber, elements)
 
     // ==================== 3. 返回下一个工作单元（fiber） =====================
     if (fiber.child) {
@@ -100,31 +219,11 @@ function performUnitOfWork(fiber) {
     // 如果循环结束都还是没有找到下一个工作单元，那么就会直接返回undefined，那么workLoop就会停止
     return undefined;
 }
-
-// -----------------------（同步）提交阶段----------------------
-
-// 提交根Fiber
-function commitRoot() {
-    const now = performance.now()
-    commitWork(workInProgressRoot.child)
-    workInProgressRoot = null // 清空   
-    console.log(`commit阶段耗时：${performance.now() - now}ms`);
-}
-
-// 从 fiber 开始提交
-function commitWork(fiber) {
-    if (!fiber) return;
-
-    const parentDOM = fiber.parent.dom
-    parentDOM.appendChild(fiber.dom)
-    // 递归 child 和 sibling
-    commitWork(fiber.child)
-    commitWork(fiber.sibling)
-}
-// --------------------------------------------------------------
+// ---------------------------------------------------------------------
 
 
-// 工作循环
+
+// -------------------------- （7）工作循环 ----------------------------
 // 这个deadLine是requestIdleCallback给的
 function workLoop(deadLine) {
     let shouldYield = false
@@ -142,12 +241,9 @@ function workLoop(deadLine) {
         commitRoot()
     }
 }
-
 // 第一次请求
 requestIdleCallback(workLoop)
-
-
-
+// ---------------------------------------------------------------
 
 
 
--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const element = MiniReact.createElement(
     'h1',
     { id: 'title', style: "background:red" },
     MiniReact.createTextElement('Hello World'),
-    MiniReact.createElement('div', { id: 'name', style: "background:blue;" }, '—MiniReact')
+    MiniReact.createElement('div', { id: 'name', style: "background:blue;" }, '—MiniReact'),
+    MiniReact.createElement('button', { onClick: () => alert('hi') }, 'Click me!')
 )
 
 const container = document.querySelector('#root');
